4760d2e47fbc12fc28245929f10db1c2a8bffba5
nishant <nishant.singh87@gmail.com>
2018-02-09 22:24:33 +0530

    Create initial schema.

---
+++ db/migrate/20180209164247_intial_schema.rb
@@ -0,0 +1,21 @@
+class IntialSchema < ActiveRecord::Migration[5.0]
+  def change
+    create_table :categories do |t|
+      t.string :name
+    end
+
+    create_table :subcategories do |t|
+      t.string :name
+      t.integer :category_id
+    end
+
+    create_table :listings do |t|
+      t.string :title
+      t.text   :description
+      t.string :city
+      t.string :state
+      t.string :pincode
+      t.timestamps
+    end
+  end
+end
